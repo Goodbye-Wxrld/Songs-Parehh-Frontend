3a455430fd3bb220f70c1b0a213bf5c9a7af66e6
WZT006 <60822592+WZT006@users.noreply.github.com>
2023-02-01 12:40:11 +0800

    scuffed google signin

--- src/pages/Start.js
+++ src/pages/Start.js
@@ -1,12 +1,21 @@
 import React from 'react';
-
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import { GoogleLogin } from '@react-oauth/google';
 function Start() {
     return (
         <div className="Start">
-            <h1>Start</h1>
-            <header className="App-header">
-            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSes8kfKsCwvO7vJkRSwnoivMhSa7QGeEj36E5sC_MO9IfSZIA/viewform?embedded=true" width="640" height="382" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
-            </header> 
+            
+            <GoogleOAuthProvider clientId="186308992057-ltsrtcd39881hbdkm2pl91n20i97ntbm.apps.googleusercontent.com">
+                <GoogleLogin
+                    onSuccess={credentialResponse => {
+                        console.log(credentialResponse);
+                    }}
+                    onError={() => {
+                        console.log('Login Failed');
+                    }}
+                    />
+            </GoogleOAuthProvider>
+            
         </div>
     );
 }
