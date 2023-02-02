17b7eb15f6ddf5dfc55cb014d220e621cf21a067
WZT006 <60822592+WZT006@users.noreply.github.com>
2023-02-02 13:16:48 +0800

    added some text in instructions
    forgot how to css

--- src/pages/Instructions.js
+++ src/pages/Instructions.js
@@ -2,16 +2,16 @@ import React from "react";
 
 function Instructions() {
     return (
-        <div>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget lectus justo. Etiam justo turpis, lacinia sit amet elementum nec, euismod nec ipsum. Vestibulum euismod eget lacus eget mattis. Vivamus metus mauris, molestie in erat ac, tincidunt rhoncus odio. Fusce magna est, tincidunt eget erat ut, vulputate interdum nulla. Nulla a arcu enim. Aliquam fermentum vitae lorem ac ultricies. Ut ornare ultricies urna, at bibendum dolor laoreet et. Pellentesque pellentesque lorem id lorem pulvinar auctor. In rhoncus pretium ullamcorper. Mauris laoreet ligula in lacus posuere rhoncus. Sed consequat dignissim nisi, a rhoncus nulla ultricies sit amet. Donec nec nibh ornare, rutrum orci a, commodo magna. Cras fermentum diam vel ante rhoncus semper.
-
-Aenean consequat lectus feugiat ex tristique tempus. Phasellus sodales ligula eu ipsum eleifend congue. Nunc finibus consequat massa. Proin eu ultrices metus. Phasellus ut massa sed magna semper facilisis. Nulla nulla sapien, molestie sed dolor a, dictum hendrerit nisi. Nam scelerisque egestas neque sed porta. Quisque id urna elit. Quisque eget neque sollicitudin, pulvinar velit at, sodales metus. Duis aliquam dictum dolor, molestie mattis dolor molestie gravida.
-
-Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque sit amet turpis interdum, laoreet tortor varius, molestie dolor. Cras sodales purus eu aliquam maximus. Maecenas blandit lobortis turpis ut eleifend. Praesent et imperdiet nulla. Ut consectetur posuere ipsum et viverra. Donec ultrices condimentum condimentum. Suspendisse eu ornare ante.
-
-Quisque vitae velit vitae massa pellentesque pellentesque sed ac lorem. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce pellentesque finibus convallis. Donec viverra, augue sed pulvinar convallis, dui sem venenatis sem, id accumsan justo est ac augue. Maecenas euismod vestibulum tempus. Praesent convallis elementum condimentum. Cras consectetur sodales accumsan. Nullam et dictum dolor, a maximus mauris. Donec in sapien ut ligula mollis malesuada. Sed auctor massa aliquet sem vehicula, vitae dapibus libero ornare. Integer vel turpis sagittis, elementum quam at, volutpat urna. Pellentesque id orci sed risus euismod consequat vitae nec metus. Nulla volutpat lobortis metus sit amet porttitor. Maecenas massa lorem, sodales vel maximus quis, interdum vitae metus.
-
-Duis varius pulvinar velit, et faucibus ligula convallis eu. Vivamus tristique hendrerit iaculis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Duis sapien ligula, iaculis ut odio in, aliquam facilisis diam. Nulla a enim magna. Praesent ultricies fermentum libero at elementum. Morbi non ante in mi commodo tempus faucibus sed sapien. Nunc sit amet elit sed tortor mattis tristique. Suspendisse lorem magna, elementum et semper id, semper ac massa. In finibus finibus arcu, eu luctus sem.
+        <div className="Instructions" style={ {display: 'flex' ,justifyContent: 'center'}}>
+            <h1>Instructions</h1>
+            Tasks:
+            <ul>
+                <li>Read and answer on the Consent form located <a href="https://forms.gle/6GAqKtcdyXbdRp9J7">here</a> </li>
+                <li>Listen to music piece in the following pages</li>
+                <li>After listening to the music answer which emotions did you feel while listening to the music</li>
+                <li>Confirm your answer and move to the next music piece</li>
+            </ul>
+                <button> Next</button>
         </div>
     );
 } 
--- src/pages/Start.js
+++ src/pages/Start.js
@@ -1,21 +1,27 @@
 import React from 'react';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import { GoogleLogin } from '@react-oauth/google';
+import Logo from '../vinyl-record.png';
 function Start() {
     return (
-        <div className="Start">
-            
-            <GoogleOAuthProvider clientId="186308992057-ltsrtcd39881hbdkm2pl91n20i97ntbm.apps.googleusercontent.com">
-                <GoogleLogin
-                    onSuccess={credentialResponse => {
-                        console.log(credentialResponse);
-                        console.log("Success!");
-                    }}
-                    onError={() => {
-                        console.log('Login Failed');
-                    }}
-                    />
-            </GoogleOAuthProvider>
+        
+        <div className="Start" style={{display: 'flex', justifyContent: 'center', margin: 'auto'}}>
+            <div>
+                <img src={Logo} width={200} height={200} alt="Logo Image" />
+                <GoogleOAuthProvider clientId="186308992057-ltsrtcd39881hbdkm2pl91n20i97ntbm.apps.googleusercontent.com">
+                    <GoogleLogin
+                        onSuccess={credentialResponse => {
+                            console.log(credentialResponse);
+                            console.log("Success!");
+                        }}
+                        onError={() => {
+                            console.log('Login Failed');
+                        }}
+                        />
+                </GoogleOAuthProvider>
+            </div>
+
+
             
         </div>
     );
